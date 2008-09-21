127d9a5f69f9f8a6ad29581d890cc7215fb4c465
palesz <none@none.com>
2008-09-21 09:19:11 +0000

    DragBehaviour and Sample project

--- WPFExtensions/WPFExtensions/Helpers/ExtLogicalTreeHelper.cs
+++ WPFExtensions/WPFExtensions/Helpers/ExtLogicalTreeHelper.cs
@@ -8,6 +8,15 @@ namespace WPFExtensions.Helpers
 {
 	public static class ExtLogicalTreeHelper
 	{
+		/// <summary>
+		/// Finds the first ancestor of type <typeparamref name="T"/> in the LogicalTree
+		/// starting from the <paramref name="obj"/> all way up.
+		/// It checks the Parent and the TemplatedParent (if the <code>Parent == null</code>).
+		/// </summary>
+		/// <typeparam name="T">The type of the ancestor.</typeparam>
+		/// <param name="obj">The object where we want to start.</param>
+		/// <returns>The ancestor which have been found or <code>null</code> if there's 
+		/// no ancestor with the type <typeparamref name="T"/>.</returns>
 		public static T GetAncestorOfTypeExt<T>( this DependencyObject obj )
 			where T : DependencyObject
 		{
@@ -25,6 +34,14 @@ namespace WPFExtensions.Helpers
 			return result;
 		}
 
+		/// <summary>
+		/// Finds the first ancestor of type <typeparamref name="T"/> in the LogicalTree
+		/// starting from the <paramref name="obj"/> all way up, following the Parent property.
+		/// </summary>
+		/// <typeparam name="T">The type of the ancestor.</typeparam>
+		/// <param name="obj">The object where we want to start.</param>
+		/// <returns>The ancestor which have been found or <code>null</code> if there's 
+		/// no ancestor with the type <typeparamref name="T"/>.</returns>
 		public static T GetAncestorOfType<T>( this DependencyObject obj )
 			where T : DependencyObject
 		{
@@ -37,6 +54,17 @@ namespace WPFExtensions.Helpers
 			return result;
 		}
 
+		/// <summary>
+		/// Finds the first ancestor of type <typeparamref name="T"/> in the LogicalTree
+		/// starting from the <paramref name="obj"/> all way up, following the 
+		/// Parent/TemplatedParent property.
+		/// </summary>
+		/// <typeparam name="T">The type of the ancestor.</typeparam>
+		/// <param name="obj">The object where we want to start.</param>
+		/// <param name="followTemplatedParent">If <code>true</code> the search follows the 
+		/// TemplatedParent property, if the Parent is null.</param>
+		/// <returns>The ancestor which have been found or <code>null</code> if there's 
+		/// no ancestor with the type <typeparamref name="T"/>.</returns>
 		public static T GetAncestorOfType<T>( this DependencyObject obj, bool followTemplatedParent )
 			where T : DependencyObject
 		{
