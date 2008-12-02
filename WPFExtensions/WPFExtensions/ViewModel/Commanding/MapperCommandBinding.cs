69c48f86570927eb5af9166d8ec5b5c1e05ad23e
palesz <none@none.com>
2008-12-02 01:57:52 +0000

    MapperCommandBinding refactor and sample

--- WPFExtensions/WPFExtensions/ViewModel/Commanding/MapperCommandBinding.cs
+++ WPFExtensions/WPFExtensions/ViewModel/Commanding/MapperCommandBinding.cs
@@ -10,30 +10,27 @@ namespace WPFExtensions.ViewModel.Commanding
 	public class MapperCommandBinding : CommandBinding
 	{
 
-		private ICommand _mappedCommand = null;
-
-		private bool mapped = false;
+		private ICommand _mappedToCommand = null;
 
 		/// <summary>
 		/// The command which will executed instead of the 'Command'.
 		/// </summary>
-		public ICommand MappedCommand
+		public ICommand MappedToCommand
 		{
-			get { return _mappedCommand; }
+			get { return _mappedToCommand; }
 			set
 			{
 				//mapped command cannot be null
 				if ( value == null )
 					throw new ArgumentException( "value" );
 
-				if ( mapped )
+				if ( _mappedToCommand != null )
 				{
 					this.CanExecute -= OnCanExecute;
 					this.Executed -= OnExecuted;
 				}
 
-				this._mappedCommand = value;
-				mapped = true;
+				this._mappedToCommand = value;
 
 				this.CanExecute += OnCanExecute;
 				this.Executed += OnExecuted;
@@ -46,20 +43,20 @@ namespace WPFExtensions.ViewModel.Commanding
 		//
 		protected void OnCanExecute( object sender, CanExecuteRoutedEventArgs e )
 		{
-			if ( MappedCommand is RoutedCommand && e.Source is IInputElement )
-				e.CanExecute = ( MappedCommand as RoutedCommand ).CanExecute( e.Parameter, e.Source as IInputElement );
+			if ( MappedToCommand is RoutedCommand && e.Source is IInputElement )
+				e.CanExecute = ( MappedToCommand as RoutedCommand ).CanExecute( e.Parameter, e.Source as IInputElement );
 			else
-				e.CanExecute = MappedCommand.CanExecute( e.Parameter );
+				e.CanExecute = MappedToCommand.CanExecute( e.Parameter );
 			e.Handled = true;
 			e.ContinueRouting = false;
 		}
 
 		protected void OnExecuted( object sender, ExecutedRoutedEventArgs e )
 		{
-			if ( MappedCommand is RoutedCommand && e.Source is IInputElement )
-				( MappedCommand as RoutedCommand ).Execute( e.Parameter, e.Source as IInputElement );
+			if ( MappedToCommand is RoutedCommand && e.Source is IInputElement )
+				( MappedToCommand as RoutedCommand ).Execute( e.Parameter, e.Source as IInputElement );
 			else
-				MappedCommand.Execute( e.Parameter );
+				MappedToCommand.Execute( e.Parameter );
 			e.Handled = true;
 		}
 	}
